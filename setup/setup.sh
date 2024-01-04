60e8d529d939a8522d374ead93d71ba68150e31e
Jonathan Henrich <henrich1@phobos.cidas.uni-goettingen.de>
2024-01-04 15:16:12 +0100

    minor pathfixing

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,7 +8,7 @@ conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit
 conda install -c conda-forge timm==0.6.12 -y
 conda install -c conda-forge tensorboard -y
 conda install -c conda-forge tensorboardx -y 
-pip install -r requirements.txt
+pip install -r setup/requirements.txt
 
 
 # build
